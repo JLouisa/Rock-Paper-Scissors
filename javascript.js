7c4a7b8b9e697a88fe79c024f7023e57ebe2cae5
Jonathan Louisa <jonathan.louisa@gmail.com>
2023-07-20 09:00:02 -0400

    Finished UI and game logic
    I added a forced page refresh after the player lost or won 5 games.
    Ties are not counted.

--- javascript.js
+++ javascript.js
@@ -6,22 +6,45 @@ const announce = document.querySelector('#announcement');
 const computerShow = document.querySelector('#computerChoice');
 let player;
 
+  //Rounds
+let gameWin = 0;
+let gameLose = 0;
+let gameTie = 0;
+let roundB = 1;
+let result;
+
+const round = document.querySelector('.roundBoard');
+round.textContent = roundB;
+
+const won = document.querySelector('.wonBoard');
+won.textContent = gameWin;
+
+const lose = document.querySelector('.loseBoard');
+lose.textContent = gameLose;
+
+function roundsCount() {
+        if(gameWin > 4) {alert("You WON against the Computer!"); location.reload()};
+        if(gameLose > 4) {alert("You LOST against the Computer!"); location.reload()};
+    }
+
 //Player selection UI
 rock.addEventListener('click', () => {
     player = "rock";
     announce.textContent = (game());
+    roundsCount()
   });
 
 paper.addEventListener('click', () => {
     player ="paper";
     announce.textContent = (game());
+    roundsCount()
   });
 
 scissors.addEventListener('click', () => {
     player = "scissors";
     announce.textContent = (game());
+    roundsCount()
   });
-
 
 //Game logic
 function game() {
@@ -42,41 +65,14 @@ function game() {
     computerShow.textContent = choiceComp;
     
 //Game Rules
-    if(playerSelection === computerSelection) {return ("It's a tie! Try again")}
-        else {if(playerSelection ==="rock" && computerSelection === "scissors") {return ("You win! Rock beats Scissors")}
-        else {if(playerSelection ==="scissors" && computerSelection === "paper") {return ("You win! Scissors beats Paper")}
-        else {if(playerSelection ==="paper" && computerSelection === "rock") {return ("You win! Paper beats Rock")}
-        else {if(computerSelection === "rock") {return ("You lose! Rock beats Scissors")}
-        else {if(computerSelection === "scissors") {return ("You lose! Scissors beats paper")}
-        else {return ("You lose! Paper beats Rock")
+    if(playerSelection === computerSelection) {round.textContent = roundB++; return ("It's a tie! Try again")}
+        else {if(playerSelection ==="rock" && computerSelection === "scissors") {round.textContent = roundB++; won.textContent = ++gameWin; return ("You win! Rock beats Scissors")}
+        else {if(playerSelection ==="scissors" && computerSelection === "paper") {round.textContent = roundB++; won.textContent = ++gameWin; return ("You win! Scissors beats Paper")}
+        else {if(playerSelection ==="paper" && computerSelection === "rock") {round.textContent = roundB++; won.textContent = ++gameWin; return ("You win! Paper beats Rock")}
+        else {if(computerSelection === "rock") {round.textContent = roundB++; lose.textContent = ++gameLose; return ("You lose! Rock beats Scissors")}
+        else {if(computerSelection === "scissors") {round.textContent = roundB++; lose.textContent = ++gameLose; return ("You lose! Scissors beats paper")}
+        else {round.textContent = roundB++; lose.textContent = ++gameLose; return ("You lose! Paper beats Rock")
     }}}}}}
     }
 return playRound()
 }
-
-let gameWin = 0;
-let gameLose = 0;
-let gameTie = 0;
-let roundB = 0;
-let result;
-
-const round = document.querySelector('.roundBoard');
-round.textContent = roundB;
-
-const won = document.querySelector('.wonBoard');
-won.textContent = gameWin;
-
-const lose = document.querySelector('.loseBoard');
-lose.textContent = gameLose;
-
-// // Round counting
-// for(i = 1; i < 6; i++) {
-//     console.log("Round " + i);
-//     result = game()
-//     console.log(result)
-//     if(result == undefined){ i = 10}
-//     else {if(result == "You win! Rock beats Scissors" || result == "You win! Scissors beats Paper" || result == "You win! Paper beats Rock") {++gameWin}
-//     else {if(result == "You lose! Paper beats Rock" || result == "You lose! Scissors beats paper" || result == "You lose! Rock beats Scissors") {++gameLose}
-//     else {++gameTie}}};
-//     if(i < 6) {console.log("You won " + gameWin + " and lost " + gameLose + " with " + gameTie + " ties!")};
-// }
